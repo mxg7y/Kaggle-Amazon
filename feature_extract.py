eefadb04091017ce44f57ed7810ad97f2a821cef
Kamiura Akiyuki <bxp5dkrmrz8ce@gmail.com>
2017-07-13 14:08:37 +0900

    feature extract

---
+++ feature_extract.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+import numpy as np
+from skimage.feature import hog
+
+'''
+    Feature extraction from np.array
+'''
+def hog_cnl(image, channels=3, ):
+    img_cnl = [ image[:, :, i] for i in range(channels)]
+    img_hog = np.array([ hog(cnl, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(3, 3)) for cnl in img_cnl])
+    return img_hog.flatten()
+
+def bovw(image):
+    pass
--- test-k.py
+++ test-k.py
@@ -11,12 +11,3 @@ from skimage.feature import hog
 
 # train_csv = pd.read_csv('input_datas/train_v2.csv')
 # print(train_csv)
-
-input_file_path = os.path.join(os.path.dirname(__file__), 'input_file/*.jpg')
-filepaths = glob.glob(input_file_path)
-print(filepaths)
-imgs = [io.imread(f) for f in filepaths]
-for img in imgs:
-    img_cnl = img[:,:,0], img[:,:,1], img[:,:,2]
-    img_hog = np.array([ hog(cnl, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(3, 3)) for cnl in img_cnl])
-    img_hog = img_hog.flatten()
